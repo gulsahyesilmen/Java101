c432f0ac1702b1a1d7a8de33cb5ded850a502efb
Gülşah <gulsahyesilkurt@gmail.com>
2023-10-14 00:51:30 +0300

    Initial commit

---
+++ src/MultiplesOfFour.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class MultiplesOfFour {
+    public static void main(String[] args){
+
+        //Define variables
+        int n, total = 0;
+
+        //Define Scanner Class
+        Scanner inp = new Scanner(System.in);
+
+        //Ask user for the number
+            do{
+                System.out.print("Bir sayı giriniz : ");
+                n = inp.nextInt();
+                   if(n%4 == 0){
+                      total += n;
+                   }
+            }while(n%2==0);
+
+        System.out.println("Toplam = " +total);
+
+    }
+}
